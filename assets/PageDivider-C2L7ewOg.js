06775be9ed03c29f387b12d221d70943912a4c0c
görkem serin <gorkemserin@outlook.com>
2025-03-21 12:27:11 +0300

    Updates

---
+++ assets/PageDivider-C2L7ewOg.js
@@ -0,0 +1,1 @@
+import{r as a,j as s}from"./index-LcgpnUoA.js";const e="/OakTravel/assets/Logo-D7GNiX99.webp",o=a.memo(function(){return s.jsx("div",{className:"flex w-full flex-col my-8 md:my-16 p-2 md:px-4 lg:px-0 gap-4 z-10 lg:max-w-9/12 mx-auto",children:s.jsx("div",{className:"divider",children:s.jsx("img",{src:e,loading:"lazy",className:"w-24 z-10"})})})});export{o as default};
